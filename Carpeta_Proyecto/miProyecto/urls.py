9427a333dee6b693a6bada6751c2c85ced2595f1
Marco Girardi <marco7girardi@gmail.com>
2025-02-18 22:04:58 -0300

    Crear un template

--- Carpeta_Proyecto/miProyecto/urls.py
+++ Carpeta_Proyecto/miProyecto/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import saludo
+from miapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/', saludo),
+    path('finalizar/', views.index),
 ]
--- Carpeta_Proyecto/miapp/views.py
+++ Carpeta_Proyecto/miapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    gracias = {"mensaje": "Gracias por visitar mi página"}
+    return render(request, 'miapp/index.html', gracias)
